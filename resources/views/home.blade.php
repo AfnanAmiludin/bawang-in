e0bb512c588cfc5c54073f4840736bcb487ad075
Afnan Amiludin <amldn.afnn@gmail.com>
2023-09-29 18:55:44 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BawangIn</title>
+        <link rel="stylesheet" href="{{asset('asset/css/home.css')}}">
+    </head>
+    <body>
+        <nav>
+            <div class="logo">
+                <a href="#"><img src="{{asset('asset/image/bawangIn.png')}}" alt=""></a>
+            </div>
+            <ul>
+                <li><a class="page" href="#">Home</a></li>
+                <li><a class="page" href="#">Home</a></li>
+                <li><a class="page" href="#">Home</a></li>
+            </ul>
+            <div class="icon">
+                <a class="icon-a" href="#"><img src="{{asset('asset/icon/shop.png')}}" alt=""></a>
+                <a class="icon-b" href="#"><img src="{{asset('asset/icon/notif.png')}}" alt=""></a>
+                <a class="icon-c" href="#"><img src="{{asset('asset/icon/person.png')}}" alt=""></a>
+            </div>
+        </nav>
+        <head>
+            <div class="background-head">
+                <div class="container-head-1" >
+                    <div class="content-1"><h2>Fresh</h2></div>
+                    <div class="content-2"><h1>Bawang Merah dan Bawang Putih</h1></div>
+                    <div class="content-3"><h3>Menyediakan Bawang Merah dan Bawang Putih dengan kualitas terbaik dan harga terjangkau.</h1></div>
+                </div>
+                <div class="container-head-2" >
+                    <h2></h2>
+                </div>
+            </div>
+        </head>
+    </body>
+</html>
